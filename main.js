0110c27022e99f60198c6d1f49d6474dcf07b3ed
Bohdan Levantovych <levantovychbogdan@gmail.com>
2022-11-27 13:24:30 +0000

    refactor

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 (() => {
+	const $ = document.querySelector;
+	const $$ = document.querySelectorAll;
+	const event = $('h2.title').textContent;
+	const round = $('h2.subTitle').textContent.replace('Round ', '');
+	const result = $$('div.score').reverse().map(scoreEl => scoreEl.textContent).join('-').replace(/½/g, '1/2');
 
+	const names = [];
+	const elos = [];
+	const moves = [];
 
-	const event = $('h2.title').text();
-	const round = $('h2.subTitle').text().replace('Round ', '');
-	const result = $('div.score').slice(0, 2).text().split('').reverse().join('')
-					.replace(/(.)(.)/g, (str, w, b) => { return w + '-' + b }).replace(/½/g, '1/2');
-
-	let names = [];
-	let elos  = [];
-	let moves = [];
-
-	$('h2.name').slice(0,2).each(function() {
-    	names.push($(this).text().replace(/ /g, ''));
+	$$('h2.name').slice(0, 2).each(function () {
+		names.push($(this).text().replace(/ /g, ''));
 	});
 
-	$('span.elo').slice(0, 2).each(function() {
+	$('span.elo').slice(0, 2).each(function () {
 		elos.push($(this).text());
 	});
 
@@ -22,9 +21,9 @@
 
 		let pasteMove = true;
 		let moveNum = 1;
-		$('span.move').each(function() {
+		$('span.move').each(function () {
 			if (pasteMove) {
-				if (moveNum % 6 === 0){
+				if (moveNum % 6 === 0) {
 					moves.push('\n' + moveNum.toString() + '.');
 				}
 				else {
@@ -36,27 +35,35 @@
 			} else {
 				pasteMove = true;
 			}
-    		moves.push($(this).text().trim());
+			moves.push($(this).text().trim());
 		});
 		moves = moves.join(' ');
 	})();
 
 
-	let pgn = `[Event "${event}"]\n[Round "${round}"]\n[Result "${result}"]\n[White "${names[1]}"]\n[Black "${names[0]}"]\n[WhiteElo "${elos[1]}"]\n[BlackElo "${elos[0]}"]\n\n${moves} ${result}`;
+	const pgn = `[Event "${event}"]
+[Round "${round}"]
+[Result "${result}"]
+[White "${names[1]}"]
+[Black "${names[0]}"]
+[WhiteElo "${elos[1]}"]
+[BlackElo "${elos[0]}"]
+
+${moves} ${result}`;
 
 	(() => {
-		let filename = names[1] + '-' + names[0] + '.pgn';
-		let blob = new Blob([pgn], {type: 'text/plain'}),
-	    	e    = document.createEvent('MouseEvents'),
-	    	a    = document.createElement('a');
+		const filename = `${names[1]}-${names[0]}.pgn`;
+		const blob = new Blob([pgn], { type: 'text/plain' }),
+			e = document.createEvent('MouseEvents'),
+			a = document.createElement('a');
 
-	    a.download = filename;
-	    a.href = window.URL.createObjectURL(blob);
-	    a.dataset.downloadurl =  ['text/plain', a.download, a.href].join(':');
-	    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-	    a.dispatchEvent(e);
+		a.download = filename;
+		a.href = window.URL.createObjectURL(blob);
+		a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
+		e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+		a.dispatchEvent(e);
 	})();
-	
+
 	// copy to clipboard
 	(() => {
 		const input = document.createElement('input');
